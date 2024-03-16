9eec67a7b57c3e6afe5c6da1f1577643174a8000
medaparedd <medapareddysudhasri@gmail.com>
2024-03-16 11:09:44 +0530

    vpc

--- provider.tf
+++ provider.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.31.0" # AWS provider version, not terraform version
+      source  = "hashicorp/aws"
+      version = "5.31.0" # eks module requires more than 5.38
     }
+  }
+
+  backend "s3" {
+    bucket         = "daws74s-state-dev"
+    key            = "workstation"
+    region         = "us-east-1"
+    dynamodb_table = "daws74s-locking-dev"
   }
 }
 
